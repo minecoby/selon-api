d64a896d9303bf6d6e7955b0dc22de2e0e9e8483
minecoby <minecoby@naver.com>
2024-05-13 00:34:58 +0900

    ㅇ

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ items = {"1": {"name" : "pen"}, "2":{"name":"pencil"}}
 
 @app.get("/items")
 async def read_items(): 
-    logger.info("Fetching all items adddd")
+    logger.info("Fetching all items addd")
     return items
 
 
