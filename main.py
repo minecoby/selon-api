080c2b68b2a4495858a5ebf181d4157d0d01c6b8
minecoby <minecoby@naver.com>
2024-05-13 00:20:24 +0900

    ㅇ

--- main.py
+++ main.py
@@ -24,29 +24,17 @@ app.add_middleware(
 
 WEBHOOK_SECRET = "74D55CAF58DFEF548645C15FA8EA4"
 
-
-
-def verify_signature(x_hub_signature, data):
-    """
-    GitHub에서 전송된 X-Hub-Signature 헤더와 요청 데이터를 사용하여 HMAC SHA1 서명을 검증합니다.
-    """
-    github_signature = hmac.new(bytes(WEBHOOK_SECRET, 'utf-8'), msg=data, digestmod=hashlib.sha1).hexdigest()
-    return hmac.compare_digest(f'sha1={github_signature}', x_hub_signature)
-
 @app.post("/webhook/")
-async def github_webhook(request: Request, x_hub_signature):
-    body = await request.body()
-
-    # 시그니처 검증
-    if x_hub_signature is None or not verify_signature(x_hub_signature, body):
-        raise HTTPException(status_code=400, detail="Invalid signature")
-
+async def github_webhook(request: Request):
     webhook_data = await request.json()
     # 여기서 webhook_data를 사용하여 특정 이벤트(예: push 이벤트)에 대한 처리를 할 수 있습니다.
+    # 예를 들어, 특정 브랜치에 대한 push 이벤트만 처리하고 싶다면 여기서 필터링할 수 있습니다.
 
     # push 이벤트가 감지되면 로컬에서 git pull 실행
     subprocess.run(["git", "pull"], cwd="/path/to/your/local/repo")
     return {"message": "Successfully pulled."}
+
+
 
 #=========================================================================================================================================================
 @app.get("/") #데코레이터
