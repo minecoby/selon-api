179c12f61190287a0d9f1ee0795e91ed8c6a702d
이지원 <superjiwon11@gmail.com>
2024-05-15 03:54:34 +0900

    notices.py 수정

--- notices.py
+++ notices.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import Session
 from pydantic import BaseModel
 from typing import List
