3513897cff43cd00ffd347e57a40ecef7af35577
이지원 <superjiwon11@gmail.com>
2024-05-23 04:05:14 +0900

    front 삽질

--- frontend/SelonApiApp/App.js
+++ frontend/SelonApiApp/App.js
@@ -1,11 +1,54 @@
-import { enableScreens } from 'react-native-screens';
-enableScreens();
+import React, { useState, useEffect } from 'react';
+import { View, Image, StyleSheet, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './src/Home';
+import Login from './src/Login';
+import Register from './src/Register';
+import image from './src/assets/selon_logo.png'; // 이미지 파일 경로 설정
 
-import React from 'react';
-import AuthStack from './src/navigation/AuthStack';
+const Stack = createStackNavigator();
 
 const App = () => {
-  return <AuthStack />;
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
+  if (loading) {
+    return (
+      <View style={styles.loadingContainer}>
+        <Image source={image} style={styles.logo} />
+      </View>
+    );
+  }
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
+        <Stack.Screen name="Register" component={Register} options={{ headerShown: false }} />
+        <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
+
+const styles = StyleSheet.create({
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#292929',
+  },
+  logo: {
+    width: 200,
+    height: 200,
+    resizeMode: 'contain',
+  },
+});
 
 export default App;
