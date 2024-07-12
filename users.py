414f815a0dd80452f3d5ddda9b07065628f488fd
minecoby <minecoby@naver.com>
2024-07-12 12:47:38 +0900

    ㅇㅇ

--- users.py
+++ users.py
@@ -21,23 +21,15 @@ from database import user_Base, get_userdb, user_engine
 class User(user_Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(String(255), unique=True, index=True)
+    username = Column(String(255), unique=True, index=True)
     hashed_password = Column(String(255))
     nickname = Column(String(255), unique=True, index=True, nullable=True)
-    realname = Column(String(255), unique=True, index=True, nullable=True)
     grade = Column(Integer, nullable=True)
 
 user_Base.metadata.create_all(bind=user_engine)
 
 class UserCreate(BaseModel):
-    user_id: str
-    realname: str
-    nickname: str
-    password: str
-    grade: int
-
-class UserLogin(BaseModel):
-    user_id: str
+    username: str
     password: str
 
 class UserUpdate(BaseModel):
@@ -52,11 +44,19 @@ class UserResponseLogin(BaseModel):
         from_attributes = True
 
 class UserResponse(BaseModel):
+<<<<<<< HEAD
     user_id: str
     realname: str
     nickname: str
     password: str
     grade: int
+=======
+    id: int
+    username: str
+    nickname: Optional[str] = None
+    grade: Optional[int] = None
+
+>>>>>>> parent of 27e341b9 (로그인 모델 수정)
     class Config:
         from_attributes = True
 
@@ -70,6 +70,7 @@ def get_password_hash(password):
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
+<<<<<<< HEAD
 def create_access_token(data: dict, expires_delta: timedelta = None):
     to_encode = data.copy()
     expire = datetime.utcnow() + expires_delta if expires_delta else datetime.utcnow() + timedelta(minutes=15)
@@ -80,14 +81,14 @@ def create_access_token(data: dict, expires_delta: timedelta = None):
 
 
 @router.post("/users/signup", response_model=UserResponse, tags=["user"])
+=======
+@router.post("/users/", response_model=UserResponse, tags=["user"])
+>>>>>>> parent of 27e341b9 (로그인 모델 수정)
 def create_user(user: UserCreate, db: Session = Depends(get_userdb)):
     hashed_password = get_password_hash(user.password)
     db_user = User(
-        user_id=user.user_id,
-        realname = user.realname,
-        nickname = user.nickname,
+        username=user.username,
         hashed_password=hashed_password,
-        grade = user.grade
     )
     try:
         db.add(db_user)
@@ -95,9 +96,10 @@ def create_user(user: UserCreate, db: Session = Depends(get_userdb)):
         db.refresh(db_user)
     except IntegrityError:
         db.rollback()
-        raise HTTPException(status_code=400, detail="user_id already registered")
+        raise HTTPException(status_code=400, detail="Username already registered")
     return db_user
 
+<<<<<<< HEAD
 @router.post("/users/login", response_model=UserResponseLogin, tags=["user"])
 def login_user(user: UserLogin, db: Session = Depends(get_userdb)):
     db_user = db.query(User).filter(User.user_id == user.user_id).first()
@@ -113,6 +115,14 @@ def login_user(user: UserLogin, db: Session = Depends(get_userdb)):
         "access_token": access_token,
         "token_type": "bearer"
     }
+=======
+@router.post("/users/login", response_model=UserResponse, tags=["user"])
+def login_user(user: UserCreate, db: Session = Depends(get_userdb)):
+    db_user = db.query(User).filter(User.username == user.username).first()
+    if db_user is None or not verify_password(user.password, db_user.hashed_password):
+        raise HTTPException(status_code=400, detail="Invalid username or password")
+    return db_user
+>>>>>>> parent of 27e341b9 (로그인 모델 수정)
 
 @router.patch("/users/{user_id}", response_model=UserResponse, tags=["user"])
 def update_user(user_id: int, user_update: UserUpdate, db: Session = Depends(get_userdb)):
