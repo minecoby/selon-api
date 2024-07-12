beb0008514853ee5709b74750ecea18e3347349d
minecoby <minecoby@naver.com>
2024-07-12 12:39:57 +0900

    토큰지급 추가

--- users.py
+++ users.py
@@ -1,11 +1,21 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException,Security
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import IntegrityError
 from pydantic import BaseModel
 from passlib.context import CryptContext
 from typing import Optional
+import jwt
+from datetime import datetime, timedelta
+from dotenv import load_dotenv
+import os
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 
+load_dotenv()
+
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.environ.get("ACCESS_TOKEN_EXPIRE_MINUTES"))
+SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
+ALGORITHM = os.environ.get("ALGORITHM")
 from database import user_Base, get_userdb, user_engine
 
 class User(user_Base):
@@ -35,12 +45,9 @@ class UserUpdate(BaseModel):
     grade: Optional[int] = None
 
 class UserResponse(BaseModel):
-    id: int
     user_id: str
-    realname: str
-    nickname: Optional[str] = None
-    grade: Optional[int] = None
-
+    access_token : str
+    token_type : str
     class Config:
         from_attributes = True
 
@@ -53,6 +60,15 @@ def get_password_hash(password):
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(data: dict, expires_delta: timedelta = None):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + expires_delta if expires_delta else datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
 
 @router.post("/users/signup", response_model=UserResponse, tags=["user"])
 def create_user(user: UserCreate, db: Session = Depends(get_userdb)):
@@ -78,7 +94,16 @@ def login_user(user: UserLogin, db: Session = Depends(get_userdb)):
     db_user = db.query(User).filter(User.user_id == user.user_id).first()
     if db_user is None or not verify_password(user.password, db_user.hashed_password):
         raise HTTPException(status_code=400, detail="Invalid user_id or password")
-    return db_user
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": db_user.user_id}, expires_delta=access_token_expires
+    )
+
+    return {
+        "user_id": db_user.user_id,
+        "access_token": access_token,
+        "token_type": "bearer"
+    }
 
 @router.patch("/users/{user_id}", response_model=UserResponse, tags=["user"])
 def update_user(user_id: int, user_update: UserUpdate, db: Session = Depends(get_userdb)):
@@ -101,3 +126,18 @@ def read_user(user_id: int, db: Session = Depends(get_userdb)):
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
+
+
+security = HTTPBearer()
+
+@router.get("/users/validate-token")
+def validate_token(credentials: HTTPAuthorizationCredentials = Security(security), db: Session = Depends(get_userdb)):
+    token = credentials.credentials
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+        user_id = payload.get("sub")
+        if db.query(User).filter(User.user_id == user_id).first() is None:
+            raise HTTPException(status_code=404, detail="User not found")
+        return {"status": "valid", "user_id": user_id}
+    except jwt.PyJWTError as e:
+        raise HTTPException(status_code=403, detail="Invalid token")
